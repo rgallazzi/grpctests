15e8e8029d974ef9b53a0f12b3541ecfc6323028
Rodrigo Leite <roleite@microsoft.com>
2023-09-14 19:07:39 -0300

    fix .net version

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace GrpcGreeter
 
             // Additional configuration is required to successfully run gRPC on macOS.
             // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
+           // logging.AddEventSourceLogger();
             // Add services to the container.
             builder.Services.AddGrpc();
 

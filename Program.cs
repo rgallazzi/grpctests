5e6a7130597213996e8acb1a83bf472400592fc7
Rodrigo Leite <roleite@microsoft.com>
2023-09-15 18:09:42 -0300

    adding trace logging to grpc

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace GrpcGreeter
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Logging.AddConsole();
 
             // Additional configuration is required to successfully run gRPC on macOS.
             // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
